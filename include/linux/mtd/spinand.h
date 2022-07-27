7e93e36ee18b248042d632c24bea39b28e76583d
Weijie Gao <weijie.gao@mediatek.com>
2022-07-27 16:36:13 +0800

    mtd: spi-nand: add more spi-nand chips
    Support more spi-nand chips
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- include/linux/mtd/spinand.h
+++ include/linux/mtd/spinand.h
@@ -361,8 +361,10 @@ struct spinand_manufacturer {
 extern const struct spinand_manufacturer alliancememory_spinand_manufacturer;
 extern const struct spinand_manufacturer ato_spinand_manufacturer;
 extern const struct spinand_manufacturer esmt_c8_spinand_manufacturer;
+extern const struct spinand_manufacturer etron_spinand_manufacturer;
 extern const struct spinand_manufacturer fmsh_spinand_manufacturer;
 extern const struct spinand_manufacturer foresee_spinand_manufacturer;
+extern const struct spinand_manufacturer fudan_spinand_manufacturer;
 extern const struct spinand_manufacturer gigadevice_spinand_manufacturer;
 extern const struct spinand_manufacturer macronix_spinand_manufacturer;
 extern const struct spinand_manufacturer micron_spinand_manufacturer;
