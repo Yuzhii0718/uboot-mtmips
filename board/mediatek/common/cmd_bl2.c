kimocoder/u-boot

board: mediatek: add upgrading/booting framework
Add framework for mediatek-specific upgrading and booting purpose:

Bootmenus:
bootmenu_mmc: generic bootmenu for eMMC and SD card
bootmenu_mtd: generic bootmenu for SPI-NOR and NAND (UBI)

Helpers:
load_data: API for loading data into DRAM
upgrade_helper: API for reading/writing various flashes
boot_helper: API for booting image from various flashes
verify_helper: API for verifying image from memory and from flash
dual_boot: API for dual-image support
bl2_helper: API for validating BL2
fip_helper: API for validating and modifying FIP
mmc_helper: API for mmc devices including dual-image
mtd_helper: API for mtd devices including dual-image

Commands:
cmd_mtkload: load data into DRAM, and optionally boot the image data
cmd_mtkupgrade: load and write data to flash region defined by the board
cmd_mtkboardboot: boot image defined by the board
cmd_mtkautoboot: display bootmenu defined by the board

To use cmd_mtkupgrade/cmd_mtkboardboot/cmd_mtkautoboot, the boards must
enable corresponding configs.

Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

board/mediatek/common/cmd_bl2.c
@@ -1,0 +1,81 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 MediaTek Inc. All Rights Reserved.
+ *
+ * Author: Sam Shih <sam.shih@mediatek.com>
+ */
+
+#include <common.h>
+#include <command.h>
+#include "bl2_helper.h"
+#include "colored_print.h"
+
+enum bl2_cmd {
+	BL2_CHECK_IMAGE,
+};
+
+static int do_bl2_check_image(int argc, char *const argv[])
+{
+	const char *str_bl2_addr, *str_bl2_size;
+	ulong addr, size;
+
+	str_bl2_addr = *argv;
+	argc--;
+	argv++;
+
+	if (argc > 0) {
+		str_bl2_size = *argv;
+		size = simple_strtoul(str_bl2_size, NULL, 0);
+		if (!size)
+			size = MAX_BL2_SIZE;
+	}
+
+	addr = simple_strtoul(str_bl2_addr, NULL, 0);
+	if (addr) {
+		if (bl2_check_image_data((void *)addr, size)) {
+			cprintln(NORMAL, "*** BL2 verification passed ***");
+			return CMD_RET_SUCCESS;
+		}
+		cprintln(ERROR, "*** BL2 verification failed ***");
+	}
+
+	return CMD_RET_FAILURE;
+}
+
+int do_bl2(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
+{
+	const char *str_cmd;
+	enum bl2_cmd sub_cmd;
+	int ret;
+
+	if (argc < 2)
+		return CMD_RET_USAGE;
+
+	str_cmd = argv[1];
+	argc -= 2;
+	argv += 2;
+
+	switch (*str_cmd) {
+	case 'c':
+		sub_cmd = BL2_CHECK_IMAGE;
+		if (argc < 1)
+			return CMD_RET_USAGE;
+		break;
+	default:
+		return CMD_RET_USAGE;
+	}
+
+	switch (sub_cmd) {
+	case BL2_CHECK_IMAGE:
+		ret = do_bl2_check_image(argc, argv);
+		break;
+	default:
+		return CMD_RET_USAGE;
+	}
+
+	return ret;
+}
+
+U_BOOT_CMD(bl2, CONFIG_SYS_MAXARGS, 0, do_bl2, "BL2 utility commands", "\n"
+	"check <addr> [length] - check bl2 image is valid for this u-boot or not\n"
+);

Yuzhii0718/uboot-mtmips

board/mediatek/common/cmd_bl2.c
@@ -1,0 +1,81 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 MediaTek Inc. All Rights Reserved.
+ *
+ * Author: Sam Shih <sam.shih@mediatek.com>
+ */
+
+#include <command.h>
+#include <vsprintf.h>
+#include "bl2_helper.h"
+#include "colored_print.h"
+
+enum bl2_cmd {
+	BL2_CHECK_IMAGE,
+};
+
+static int do_bl2_check_image(int argc, char *const argv[])
+{
+	const char *str_bl2_addr, *str_bl2_size;
+	ulong addr, size;
+
+	str_bl2_addr = *argv;
+	argc--;
+	argv++;
+
+	if (argc > 0) {
+		str_bl2_size = *argv;
+		size = simple_strtoul(str_bl2_size, NULL, 0);
+		if (!size)
+			size = MAX_BL2_SIZE;
+	}
+
+	addr = simple_strtoul(str_bl2_addr, NULL, 0);
+	if (addr) {
+		if (!bl2_check_image_data((void *)addr, size)) {
+			cprintln(NORMAL, "*** BL2 verification passed ***");
+			return CMD_RET_SUCCESS;
+		}
+		cprintln(ERROR, "*** BL2 verification failed ***");
+	}
+
+	return CMD_RET_FAILURE;
+}
+
+int do_bl2(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
+{
+	const char *str_cmd;
+	enum bl2_cmd sub_cmd;
+	int ret;
+
+	if (argc < 2)
+		return CMD_RET_USAGE;
+
+	str_cmd = argv[1];
+	argc -= 2;
+	argv += 2;
+
+	switch (*str_cmd) {
+	case 'c':
+		sub_cmd = BL2_CHECK_IMAGE;
+		if (argc < 1)
+			return CMD_RET_USAGE;
+		break;
+	default:
+		return CMD_RET_USAGE;
+	}
+
+	switch (sub_cmd) {
+	case BL2_CHECK_IMAGE:
+		ret = do_bl2_check_image(argc, argv);
+		break;
+	default:
+		return CMD_RET_USAGE;
+	}
+
+	return ret;
+}
+
+U_BOOT_CMD(bl2, CONFIG_SYS_MAXARGS, 0, do_bl2, "BL2 utility commands", "\n"
+	"check <addr> [length] - check bl2 image is valid for this u-boot or not\n"
+);